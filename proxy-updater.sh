b63c5e288e96197bff47d3cea8457599724a84be
Aryan <83180468+Aruack@users.noreply.github.com>
2023-05-01 19:07:44 +0530

    Create proxy-updater.sh

---
+++ proxy-updater.sh
@@ -0,0 +1,31 @@
+
+echo "██╗░░░██╗██████╗░██████╗░░█████╗░████████╗██╗███╗░░██╗░██████╗░"
+echo "██║░░░██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██║████╗░██║██╔════╝░"
+echo "██║░░░██║██████╔╝██║░░██║███████║░░░██║░░░██║██╔██╗██║██║░░██╗░"
+echo "██║░░░██║██╔═══╝░██║░░██║██╔══██║░░░██║░░░██║██║╚████║██║░░╚██╗"
+echo "╚██████╔╝██║░░░░░██████╔╝██║░░██║░░░██║░░░██║██║░╚███║╚██████╔╝"
+echo "░╚═════╝░╚═╝░░░░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░"
+cd 
+cd
+rm -rf termuxproxy
+git clone https://github.com/aruack/termuxproxy
+cd AutoUpdateMyTools
+python2 What1.py
+cd 
+cd termuxproxy
+cd packages
+bash OtherInstall.sh
+sleep 1
+cd 
+cd termuxproxy
+echo "██████╗░░█████╗░███╗░░██╗███████╗"
+echo "██╔══██╗██╔══██╗████╗░██║██╔════╝"
+echo "██║░░██║██║░░██║██╔██╗██║█████╗░░"
+echo "██║░░██║██║░░██║██║╚████║██╔══╝░░"
+echo "██████╔╝╚█████╔╝██║░╚███║███████╗"
+echo "╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝"
+echo Developer: Aruack
+echo Create on: 01 05 2023
+echo "PLEASE WRITE THE COMMAND cd"
+echo "Run command: bash termuxproxy.sh"
+
